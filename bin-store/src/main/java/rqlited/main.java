5080d7c467a868be5fd675f4f9b8d1206018a7b2
caibin <1355869831@qq.com>
2021-07-21 21:55:53 +0100

    修改

--- bin-store/src/main/java/rqlited/main.java
+++ bin-store/src/main/java/rqlited/main.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        while (true) {
+   /*     while (true) {
             System.out.println("请输入：");
             Scanner inp = new Scanner(System.in);
             String str = inp.next();
             System.out.println("你输入了：" + str);
-        }
+        }*/
+
+
     }
 
 }
